a1f33de63eefcba69d0a44a8ae1ca187be8d4ff1
Victor David <36716074+viccy2@users.noreply.github.com>
2026-02-23 13:10:12 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,4 @@ declare global {
     interface User extends IUser {}
   }
 }
-
-// The "Brute Force" fix to stop the 'non-module entity' error
-// This simply tells TS: "languagedetect exists, don't worry about its shape"
-declare module 'languagedetect';
+// No 'declare module' needed for node-languagedetect!
