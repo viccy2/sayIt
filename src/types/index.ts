2ef7e8032390c4735e8454f00601c6accd84244e
Victor David <36716074+viccy2@users.noreply.github.com>
2026-02-23 12:55:38 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,17 @@ import { IUser } from '../models/user.model';
 
 declare global {
   namespace Express {
+    // This merges your MongoDB User model into the Express Request
     interface User extends IUser {}
   }
 }
 
-// Ensure libraries without @types don't break the build
-declare module 'languagedetect';
+// Fixes the "non-module entity" error for languagedetect
+declare module 'languagedetect' {
+  class LanguageDetect {
+    constructor();
+    detect(text: string, limit?: number): [string, number][];
+    getLanguages(): string[];
+  }
+  export default LanguageDetect;
+}
