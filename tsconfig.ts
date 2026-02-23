841d3eb52faaca6e2e6b7a9a32403a5271ed847e
Victor David <36716074+viccy2@users.noreply.github.com>
2026-02-23 12:32:23 +0100

    Update tsconfig.ts

--- tsconfig.ts
+++ tsconfig.ts
@@ -6,11 +6,13 @@
     "outDir": "./dist",
     "rootDir": "./src",
     "strict": true,
+    "noImplicitAny": false,
     "esModuleInterop": true,
     "skipLibCheck": true,
     "forceConsistentCasingInFileNames": true,
     "declaration": true,
-    "sourceMap": true
+    "sourceMap": true,
+    "typeRoots": ["./node_modules/@types", "./src/types"]
   },
   "include": ["src/**/*"],
   "exclude": ["node_modules", "tests", "dist"]
