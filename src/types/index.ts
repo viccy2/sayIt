6ab55c7b98442531dfc44d7c2c044521864b6d92
Victor David <36716074+viccy2@users.noreply.github.com>
2026-02-23 12:37:35 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,12 @@ import { IUser } from '../models/user.model';
 
 declare global {
   namespace Express {
+    // This merges our IUser model into the Express Request object
     interface User extends IUser {}
   }
 }
+
+// These declarations stop TSC from complaining about missing type definitions
+declare module 'languagedetect';
+declare module 'passport-google-oauth20';
+declare module 'express-session';
