207c772def1746aa4f566b774c0c5d566dfd3aec
Victor David <36716074+viccy2@users.noreply.github.com>
2026-02-24 12:01:57 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ import { IUser } from '../models/user.model';
 
 declare global {
   namespace Express {
+    // Merges your MongoDB User interface into the Express Request
     interface User extends IUser {}
   }
 }
-// No 'declare module' needed for node-languagedetect!
