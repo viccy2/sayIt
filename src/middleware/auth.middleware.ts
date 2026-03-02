168c2c0b6cd3ddff7cc95e6c51e02da6491f2fd1
Victor David <36716074+viccy2@users.noreply.github.com>
2026-03-02 11:13:28 +0100

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,8 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
 
-export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  if (req.isAuthenticated()) {
-    return next();
+export const protect = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  let token;
+  if (req.headers.authorization?.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+      req.user = await User.findById(decoded.id).select('-password') as any;
+      return next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
   }
-  res.status(401).json({ message: 'Please log in with Gmail to access this.' });
+  if (!token) return res.status(401).json({ message: 'No token' });
 };
