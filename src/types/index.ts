f0420ef70a75022c987b4260bfe5314e1708b906
Victor David <36716074+viccy2@users.noreply.github.com>
2026-03-02 11:23:21 +0100

    Delete src/types directory

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from '../models/user.model';
-
-declare global {
-  namespace Express {
-    // Merges your MongoDB User interface into the Express Request
-    interface User extends IUser {}
-  }
-}
