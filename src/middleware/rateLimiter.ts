ed298734450627449221b9711ffc4ae5a77278e0
Victor David <36716074+viccy2@users.noreply.github.com>
2026-03-09 12:52:35 +0100

    Create rateLimiter.ts

---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,17 @@
+import rateLimit from 'express-rate-limit';
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, 
+  // Customizing the response format
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).json({
+      message: options.message.message || 'Too many attempts. Please slow down.'
+    });
+  },
+  message: {
+    message: 'Too many requests. Please try again after 15 minutes.'
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
