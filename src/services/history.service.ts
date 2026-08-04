3e732c15b30d9567e2478d05c3541f36c72c7da1
Victor David <36716074+viccy2@users.noreply.github.com>
2026-08-04 03:28:20 +0100

    Update history.service.ts

--- src/services/history.service.ts
+++ src/services/history.service.ts
@@ -4,18 +4,10 @@ export const getHistoryByUserId = async (userId: string): Promise<IHistory[]> =>
   return await History.find({ user: userId }).sort({ createdAt: -1 });
 };
 
-/**
- * @desc    Delete a specific history record
- * @param   id The _id of the history record
- * @param   userId The ID of the user (to ensure ownership)
- */
 export const deleteItemById = async (id: string, userId: string): Promise<any> => {
   return await History.findOneAndDelete({ _id: id, user: userId });
 };
 
-/**
- * @desc    Create a new history record
- */
 export const createHistoryRecord = async (data: Partial<IHistory>): Promise<IHistory> => {
   return await History.create(data);
 };
