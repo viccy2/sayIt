c8b018d6ec63ad90f456b009cf70dede6e0a0e3b
Victor David <36716074+viccy2@users.noreply.github.com>
2026-08-04 03:29:14 +0100

    Update index.d.ts

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,9 +3,6 @@ import { IUser } from '../../models/user.model';
 declare global {
   namespace Express {
     interface Request {
-      /**
-       * The authenticated user object populated by the 'protect' middleware.
-       */
       user?: IUser;
     }
   }
